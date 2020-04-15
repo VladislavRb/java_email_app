5a15582707b8a3538dfb5f143a99f18c1d64f42d
unknown <vlad.raba@mail.ru>
2020-04-16 02:08:39 +0300

    modify servlet and create email utility class

--- EmailApp/src/main/java/EmailController.java
+++ EmailApp/src/main/java/EmailController.java
@@ -1,16 +1,49 @@
 package main.java;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import src.main.java.EmailUtility;
 
-@WebServlet(name = "EmailController")
+@WebServlet(name = "EmailController", urlPatterns = {"/controller"})
 public class EmailController extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private String host;
+    private String port;
+    private String user;
+    private String pass;
 
+    public void init() {
+        // reads SMTP server setting from web.xml file
+        ServletContext context = getServletContext();
+        this.host = context.getInitParameter("host");
+        this.port = context.getInitParameter("port");
+        this.user = context.getInitParameter("user");
+        this.pass = context.getInitParameter("pass");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String recipient = request.getParameter("recipient");
+        String subject = request.getParameter("subject");
+        String content = request.getParameter("content");
+
+        String resultMessage = "";
+
+        try {
+            EmailUtility.sendEmail(host, port, user, pass, recipient, subject,
+                    content);
+            resultMessage = "The e-mail was sent successfully";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            resultMessage = "There were an error: " + ex.getMessage();
+        } finally {
+            request.setAttribute("Message", resultMessage);
+            getServletContext().getRequestDispatcher("/Result.jsp").forward(
+                    request, response);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
